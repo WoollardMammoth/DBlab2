242ba460632b4ca8670c151e78974f9697335974
Griffin Cloonan <griffincloonan@gmail.com>
2018-04-13 12:31:45 -0700

    Update test script to test new commands

--- test.sh
+++ test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+## Lab 1-2 Test
+for num in {1..4}
+do 
+	echo Running Test_NR${num}
+	python schoolsearch.py -t ./Tests/Input/Test_NR${num} > ./Tests/MyOut/Test_NR${num}_MyOutput
+done 
 
+for num in {1..4}
+do 
+	echo Diff: Test_NR${num}_Output vs. Test_NR${num}_MyOutput
+	diff -y ./Tests/ExpectedOut/Test_NR${num}_Output ./Tests/MyOut/Test_NR${num}_MyOutput
+done 
+
+
+## Lab1-1 Tests
 for num in {4..11}
 do
 	echo Running Test_R${num}
@@ -9,6 +23,7 @@ done
 
 for num in {4..11}
 do 
-	echo Diff Output Test_R${num} vs. Test_R${num}_MyOutput
-	diff ./Tests/ExpectedOut/Test_R${num}_Output ./Tests/MyOut/Test_R${num}_MyOutput
+	echo Diff:  Test_R${num}_Output vs. Test_R${num}_MyOutput
+	diff -y ./Tests/ExpectedOut/Test_R${num}_Output ./Tests/MyOut/Test_R${num}_MyOutput
 done 
+
